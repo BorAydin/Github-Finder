97933c4a1ee4385aa16da8773498c63c50e00a95
Bora Aydın <boraaydins@gmail.com>
2022-09-19 04:39:08 +0300

    Add files via upload

---
+++ src/pages/About.jsx
@@ -0,0 +1,16 @@
+function About() {
+  return (
+    <div>
+      <h1 className="text-6xl mb-4">Github Finder</h1>
+      <p className="mb-4 text-2xl font-light">Github profillerini ve detaylarını gösteren bir React uygulamasıdır. Brad Traversy'in React Front To Back kursuna aittir.</p>
+      <p className="text-lg text-gray-400">
+        Versiyon <span className="text-black">1.0</span>
+      </p>
+      <p className="text-lg text-gray-400">
+        Düzen: <span className="text-black">Hassib Moddasser</span>
+      </p>
+    </div>
+  )
+}
+
+export default About
